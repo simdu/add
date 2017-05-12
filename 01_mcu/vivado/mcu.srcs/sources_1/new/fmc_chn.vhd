585a213f9188c77887cfcbe204a8a36dae7490fc
Simon Durrer <simon.durrer@gmail.com>
2017-05-12 19:37:25 +0200

    initial commit

---
+++ 01_mcu/vivado/mcu.srcs/sources_1/new/fmc_chn.vhd
@@ -0,0 +1,34 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12.05.2017 19:22:45
+-- Design Name: 
+-- Module Name: fmc_chn - rtl
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.mcu_pkg.all;
+
+entity fmc_chn is
+--  Port ( );
+end fmc_chn;
+
+architecture rtl of fmc_chn is
+
+begin
+
+
+end rtl;
---
+++ 01_mcu/vivado/mcu.srcs/sources_1/new/i_fmc_chn.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 12.05.2017 19:18:44
+-- Design Name: 
+-- Module Name: i_fmc_chn - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity i_fmc_chn is
+--  Port ( );
+end i_fmc_chn;
+
+architecture Behavioral of i_fmc_chn is
+
+begin
+
+
+end Behavioral;
