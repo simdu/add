08a5ee841195e28b1b70c5094b1267aacd8a8db3
Tobias Dubacher <t.dubacher@gmail.com>
2017-05-26 18:10:57 +0200

    add: vhdl fmc_top mit decoding, read und write

--- 01_mcu/vivado/mcu.srcs/sources_1/imports/vhdl/fmc_top.vhd
+++ 01_mcu/vivado/mcu.srcs/sources_1/imports/vhdl/fmc_top.vhd
@@ -21,26 +21,87 @@ entity fmc_top is
        -- FMC bus signals
        bus_in  : in  t_bus2rws;
        bus_out : out t_rws2bus;
-       -- FMC pin signals
-       fmc_enable : out  std_logic_vector(c_fmc_num_chn-1 downto 0);
-       fmc_direct : out  std_logic_vector(c_fmc_num_chn-1 downto 0);
-       fmc_step   : out  std_logic_vector(c_fmc_num_chn-1 downto 0)
+       
+       chn_enb :  in std_logic_vector(8-1 downto 0);
+       tmp_ctrl : in std_logic_vector(10-1 downto 0);
+       fmc_enable : out std_logic_vector(c_fmc_num_chn-1 downto 0);
+       fmc_direct : out std_logic_vector(c_fmc_num_chn-1 downto 0);
+       fmc_step   : out std_logic_vector(c_fmc_num_chn-1 downto 0)        
        );
 end fmc_top;
 
 architecture rtl of fmc_top is
     
+    -- address select signal
+    signal addr_sel : t_fmc_addr_sel;
     -- signal
-    signal chn_enb : std_logic_vector(8-1 downto 0);
-    --signal tmp_ctrl : UFIX_10_6;
+    
     signal tick_dur : std_logic;
     signal tick_dur_const : unsigned(20 downto 0);
     signal tick_nco : std_logic;
     signal tick_nco_const : unsigned(8 downto 0);
     
+    signal chn_enb_reg :  std_logic_vector(8-1 downto 0);
+    signal tmp_ctrl_reg : std_logic_vector(10-1 downto 0);
+    
     
 begin
 
+  -----------------------------------------------------------------------------
+  -- Address Decoding (combinationally)
+  -----------------------------------------------------------------------------  
+  process(bus_in.addr)
+  begin
+    case bus_in.addr is
+      -- Port 0 addresses -----------------------------------------------------
+      when c_addr_fmc_chn_enb  => addr_sel <= fmc_chn_enb;
+      when c_addr_fmc_tmp_ctrl => addr_sel <= fmc_tmp_ctrl;
+      -- unused addresses -----------------------------------------------------
+      when others                 => addr_sel <= none;
+    end case;       
+  end process;
+  
+  -----------------------------------------------------------------------------
+  -- Read Access (R and R/W registers)
+  -----------------------------------------------------------------------------  
+  P_read: process(clk)
+  begin
+    if rising_edge(clk) then
+      -- default assignment
+      bus_out.data <= (others => '0');
+      -- use address select signal
+      case addr_sel is
+        -- Port 0 registers --------------------------------------------------
+        when fmc_chn_enb  => bus_out.data(c_gpio_port_ww-1 downto 0) <= chn_enb_reg;
+        when fmc_tmp_ctrl => bus_out.data(c_gpio_port_ww-1 downto 0) <= tmp_ctrl_reg;
+        -- unused addresses ---------------------------------------------------
+        when others          => null;
+      end case;       
+    end if;      
+  end process;
+
+  -----------------------------------------------------------------------------
+  -- Write Access (R/W registers only)
+  -----------------------------------------------------------------------------  
+  P_write: process(clk, rst)
+  begin
+    if rst = '1' then
+      chn_enb_reg <= (others => '0');
+      tmp_ctrl_reg  <= (others => '0');
+    elsif rising_edge(clk) then
+      if bus_in.wr_enb = '1' then
+        -- use address select signal only in bus write cycle
+        case addr_sel is
+          when fmc_chn_enb => chn_enb_reg <= bus_in.data(c_fmc_dur_ww-1 downto 0);
+          when fmc_tmp_ctrl  => tmp_ctrl_reg  <= bus_in.data(c_fmc_dur_ww-1 downto 0);
+          -- unused addresses -------------------------------------------------
+          when others          => null;
+        end case;       
+      end if;
+    end if;
+  end process;
+  
+ 
   -----------------------------------------------------------------------------
   -- Tick Duration
   -----------------------------------------------------------------------------  
